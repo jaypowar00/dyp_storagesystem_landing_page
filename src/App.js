6b48b2edefbd84f93004c1d8fe14cd74ecf7b4a8
jaypowar00 <jaypowar00@gmail.com>
2022-05-17 17:20:20 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <div className="App" >
             <div className='title'>
                 <img src={dyp_logo} alt='logo' style={{width: '50vw', maxWidth: '400px', minWidth: '270px'}} />
+                <span style={{marginRight: '200px'}}>
+                Department of Computer Science and Engineering
+                </span>
             </div>
             <div className='content text-center'>
-                <h1 style={{display: 'inline-block'}}>
-                    CSE File&nbsp;
+                <h1 style={{display: 'inline-block', marginTop: '72px', fontSize: '75px'}}>
+                    Storage Server
                 </h1>
-                <div style={{ width: '145px', textAlign: 'left', margin: '0 auto', display: 'inline-block' }} >
-                    <TypeAnimation cursor={true} sequence={[ 'Space', 1000, 'Storage', 2000, ]} wrapper="h1" repeat={Infinity} />
-                </div>
                 <div className="content-center">
                     <AwesomeButton type="primary" ripple href="http://10.10.1.20:8086" >Get Started</AwesomeButton>
                 </div>
