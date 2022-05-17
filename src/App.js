d56ce4980478135f5a42cc63fab284ee031d17e5
jaypowar00 <jaypowar00@gmail.com>
2022-05-17 10:03:07 +0530

    img & AwesomeButton css changes

--- src/App.js
+++ src/App.js
@@ -8,27 +8,17 @@ function App() {
     return (
         <div className="App" >
             <div className='title'>
-                <img src={dyp_logo} alt='logo' style={{width: '50vw', maxWidth: '400px'}} />
+                <img src={dyp_logo} alt='logo' style={{width: '50vw', maxWidth: '400px', minWidth: '270px'}} />
             </div>
             <div className='content text-center'>
                 <h1 style={{display: 'inline-block'}}>
                     CSE File&nbsp;
                 </h1>
                 <div style={{ width: '145px', textAlign: 'left', margin: '0 auto', display: 'inline-block' }} >
-                    <TypeAnimation
-                    cursor={true}
-                    sequence={[
-                      'Space',
-                      1000,
-                      'Storage',
-                      2000,
-                    ]}
-                    wrapper="h1"
-                    repeat={Infinity}
-                  />
+                    <TypeAnimation cursor={true} sequence={[ 'Space', 1000, 'Storage', 2000, ]} wrapper="h1" repeat={Infinity} />
                 </div>
                 <div className="content-center">
-                    <AwesomeButton type="primary" ripple href="http://10.10.1.20:8086" size="large" >Get Started</AwesomeButton>
+                    <AwesomeButton type="primary" ripple href="http://10.10.1.20:8086" >Get Started</AwesomeButton>
                 </div>
                 <div className='footer'>
                   Computer Science &amp; Engineering Department<br/>
