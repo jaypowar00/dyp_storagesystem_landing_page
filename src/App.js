84ff4ebf6b7198d6364332a6517930595724c461
JayPowar <jay.powar@krishi.network>
2022-05-30 13:17:11 +0530

    responsive UI changes

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
     return (
         <div className="App" >
             <div className='title'>
-                <img src={dyp_logo} alt='logo' style={{width: '50vw', maxWidth: '400px', minWidth: '270px'}} />
-                <span style={{marginRight: '200px'}}>
+                <img src={dyp_logo} id="logo" alt='logo' />
+                <span id="cse-dept">
                 Department of Computer Science and Engineering
                 </span>
             </div>
             <div className='content text-center'>
-                <h1 style={{display: 'inline-block', marginTop: '72px', fontSize: '75px'}}>
+                <h1 id="storage-server" >
                     Storage Server
                 </h1>
                 <div className="content-center">
@@ -21,7 +21,7 @@ function App() {
                 </div>
                 <div className='footer'>
                   Computer Science &amp; Engineering Department<br/>
-                  <a href="https://github.com/RishikeshMane" title='See Rishikesh Mane on GitHub' target="_blank" rel="noreferrer" >Rishikesh Mane &#40;S.Y.&#41;</a> &amp; <a href="https://github.com/jaypowar00" title='See Jay Powar on GitHub' target="_blank" rel="noreferrer" >Jay Powar &#40;B.Tech.&#41;</a>
+                  <span>Rishikesh Mane &#40;S.Y.&#41;</span> &amp; <a href="https://github.com/jaypowar00" title='See Jay Powar on GitHub' target="_blank" rel="noreferrer" >Jay Powar &#40;B.Tech.&#41;</a>
                 </div>
             </div>
         </div>
